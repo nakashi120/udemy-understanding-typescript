e8788c48057c6805847aa88742720b60c03248ba
nakashi120 <nakashi120@gmail.com>
2021-04-15 00:32:12 +0900

    How to use ENUM type

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
+// const person: {
+//   name: string
+//   age: number
+//   hobbies: string[]
+//   role: [number, string]
+// } = {
+//   name: "Satoshi",
+//   age: 30,
+//   hobbies: ["Sports", "Cooking"],
+//   role: [2, "author"],
+// }
+// person.role[1] = 10
+// person.role.push('admin')
+// person.role = [0, 'admin', 'user']
+// console.log(person.name)
+// for (const hobby of person.hobbies) {
+//   console.log(hobby.toUpperCase())
+// }
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
 var person = {
     name: "Satoshi",
     age: 30,
-    hobbies: ["Sports", "Cooking"]
+    hobbies: ["Sports", "Cooking"],
+    role: Role.ADMIN
 };
-console.log(person.name);
-for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
-    var hobby = _a[_i];
-    console.log(hobby.toUpperCase());
+if (person.role === Role.READ_ONLY) {
+    console.log("読み取り専用ユーザー");
 }
--- app.ts
+++ app.ts
@@ -1,14 +1,39 @@
+// const person: {
+//   name: string
+//   age: number
+//   hobbies: string[]
+//   role: [number, string]
+// } = {
+//   name: "Satoshi",
+//   age: 30,
+//   hobbies: ["Sports", "Cooking"],
+//   role: [2, "author"],
+// }
+
+// person.role[1] = 10
+// person.role.push('admin')
+
+// person.role = [0, 'admin', 'user']
+
+// console.log(person.name)
+
+// for (const hobby of person.hobbies) {
+//   console.log(hobby.toUpperCase())
+// }
+
+enum Role {
+  ADMIN,
+  READ_ONLY,
+  AUTHOR,
+}
+
 const person = {
   name: "Satoshi",
   age: 30,
   hobbies: ["Sports", "Cooking"],
-  role: [2, "author"],
+  role: Role.ADMIN,
 }
 
-person.role.push("admin")
-
-console.log(person.name)
-
-for (const hobby of person.hobbies) {
-  console.log(hobby.toUpperCase())
+if (person.role === Role.READ_ONLY) {
+  console.log("読み取り専用ユーザー")
 }
