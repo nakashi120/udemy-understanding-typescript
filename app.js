bb3302b6d0488176e996057c540e3c7b86f094f6
nakashi120 <nakashi120@gmail.com>
2021-04-14 23:43:39 +0900

    How to use type of number, string, and boolean

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("ここにコードを追加します!!!");
+var add = function (n1, n2, showResult, phrase) {
+    var result = n1 + n2;
+    if (showResult) {
+        console.log(phrase + result);
+    }
+    else {
+        return result;
+    }
+};
+var number1 = 4;
+var number2 = 10;
+var printResult = true;
+var resultPhrase = "Result: ";
+add(number1, number2, printResult, resultPhrase);
