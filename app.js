49a0da6186709e951b4db2879fc0e37f524f2d56
nakashi120 <nakashi120@gmail.com>
2021-04-19 23:38:36 +0900

    union type

--- app.js
+++ app.js
@@ -1,33 +1,14 @@
-// const person: {
-//   name: string
-//   age: number
-//   hobbies: string[]
-//   role: [number, string]
-// } = {
-//   name: "Satoshi",
-//   age: 30,
-//   hobbies: ["Sports", "Cooking"],
-//   role: [2, "author"],
-// }
-// person.role[1] = 10
-// person.role.push('admin')
-// person.role = [0, 'admin', 'user']
-// console.log(person.name)
-// for (const hobby of person.hobbies) {
-//   console.log(hobby.toUpperCase())
-// }
-var Role;
-(function (Role) {
-    Role[Role["ADMIN"] = 0] = "ADMIN";
-    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
-    Role[Role["AUTHOR"] = 2] = "AUTHOR";
-})(Role || (Role = {}));
-var person = {
-    name: "Satoshi",
-    age: 30,
-    hobbies: ["Sports", "Cooking"],
-    role: Role.ADMIN
+var combine = function (input1, input2) {
+    var result;
+    if (typeof input1 === "number" && typeof input2 === "number") {
+        result = input1 + input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
 };
-if (person.role === Role.READ_ONLY) {
-    console.log("読み取り専用ユーザー");
-}
+var combinedAges = combine(30, 26);
+console.log(combinedAges);
+var combinedNames = combine("Max", "Anna");
+console.log(combinedNames);
--- app.ts
+++ app.ts
@@ -1,39 +1,15 @@
-// const person: {
-//   name: string
-//   age: number
-//   hobbies: string[]
-//   role: [number, string]
-// } = {
-//   name: "Satoshi",
-//   age: 30,
-//   hobbies: ["Sports", "Cooking"],
-//   role: [2, "author"],
-// }
-
-// person.role[1] = 10
-// person.role.push('admin')
-
-// person.role = [0, 'admin', 'user']
-
-// console.log(person.name)
-
-// for (const hobby of person.hobbies) {
-//   console.log(hobby.toUpperCase())
-// }
-
-enum Role {
-  ADMIN,
-  READ_ONLY,
-  AUTHOR,
+const combine = (input1: number | string, input2: number | string) => {
+  let result
+  if (typeof input1 === "number" && typeof input2 === "number") {
+    result = input1 + input2
+  } else {
+    result = input1.toString() + input2.toString()
+  }
+  return result
 }
 
-const person = {
-  name: "Satoshi Nakashima",
-  age: 30,
-  hobbies: ["Sports", "Cooking"],
-  role: Role.ADMIN,
-}
+const combinedAges = combine(30, 26)
+console.log(combinedAges)
 
-if (person.role === Role.READ_ONLY) {
-  console.log("読み込みユーザー")
-}
+const combinedNames = combine("Max", "Anna")
+console.log(combinedNames)
