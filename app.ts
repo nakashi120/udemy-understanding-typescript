5318cad689df466530101bd0f7006eebb478a079
nakashi120 <nakashi120@gmail.com>
2021-04-19 23:43:58 +0900

    literal type

--- app.ts
+++ app.ts
@@ -1,4 +1,8 @@
-const combine = (input1: number | string, input2: number | string) => {
+const combine = (
+  input1: number | string,
+  input2: number | string,
+  resultConversion: string
+) => {
   let result
   if (typeof input1 === "number" && typeof input2 === "number") {
     result = input1 + input2
@@ -8,8 +12,11 @@ const combine = (input1: number | string, input2: number | string) => {
   return result
 }
 
-const combinedAges = combine(30, 26)
+const combinedAges = combine(30, 26, "as-number")
 console.log(combinedAges)
 
-const combinedNames = combine("Max", "Anna")
+const combinedNames = combine("Max", "Anna", "as-number")
 console.log(combinedNames)
+
+const combinedAges = combine("30", "26", "as-text")
+console.log(combinedAges)
