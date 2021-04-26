a6d66b05ec9df463a8cc2e2b821776fe36992bc0
nakashi120 <nakashi120@gmail.com>
2021-04-26 23:22:34 +0900

    function type and void type

--- app.js
+++ app.js
@@ -1,14 +1,7 @@
-var combine = function (input1, input2) {
-    var result;
-    if (typeof input1 === "number" && typeof input2 === "number") {
-        result = input1 + input2;
-    }
-    else {
-        result = input1.toString() + input2.toString();
-    }
-    return result;
+var add = function (n1, n2) {
+    return n1 + n2;
 };
-var combinedAges = combine(30, 26);
-console.log(combinedAges);
-var combinedNames = combine("Max", "Anna");
-console.log(combinedNames);
+var printResult = function (num) {
+    console.log("Result: " + num);
+};
+console.log(printResult(add(5, 12)));
--- app.ts
+++ app.ts
@@ -1,25 +1,9 @@
-type Combinable = number | string
-type ConversionDescriptor = "as-number" | "as-text"
-
-const combine = (
-  input1: Combinable,
-  input2: Combinable,
-  resultConversion: ConversionDescriptor
-) => {
-  let result
-  if (typeof input1 === "number" && typeof input2 === "number") {
-    result = input1 + input2
-  } else {
-    result = input1.toString() + input2.toString()
-  }
-  return result
+const add = (n1: number, n2: number): number => {
+  return n1 + n2
 }
 
-const combinedAges = combine(30, 25, "as-number")
-console.log(combinedAges)
+const printResult = (num: number): void => {
+  console.log("Result: " + num)
+}
 
-const combinedNames = combine("Max", "Satoshi", "as-number")
-console.log(combinedNames)
-
-const combinedStringAges = combine("30", "25", "as-text")
-console.log(combinedAges)
+printResult(add(5, 12))
