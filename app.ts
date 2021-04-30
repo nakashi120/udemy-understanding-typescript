f53d93ba46104a51e0d18432076a0a84c1feadce
nakashi120 <nakashi120@gmail.com>
2021-04-30 23:05:59 +0900

    just committed

--- app.ts
+++ app.ts
@@ -20,6 +20,6 @@ let combineValues: (a: number, b: number) => number
 combineValues = add
 // combineValues = printResult
 
-console.log(combineValues(8, 11))
+console.log(combineValues(8, 12))
 
 printResult(add(5, 13))
